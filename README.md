43244923e94ef9e18ae1fd8eac0117fedb8f3cb0
Sanjit2000 <69010132+Sanjit2000@users.noreply.github.com>
2021-02-13 14:39:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# TowerSige2
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ box.js
@@ -0,0 +1,29 @@
+class Box {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("Green");
+      rect(pos.x, pos.y, this.width, this.height);
+
+      if(this.body.speed>3){
+        this.visiblity = this.visiblity-5
+        World.remove(world,this.body)
+        push()
+        tint(255, this.visiblity)
+        pop()
+
+     }
+    
+     
+
+    }
+  };     
